cde1c90659cfce9af1ca662393570e4a1b249d47
werick <jackericmugoma@gmail.com>
2020-04-16 07:01:32 +0300

    Added some formatting to the UI to make it presentable

--- ui.R
+++ ui.R
@@ -1,7 +1,10 @@
 #UI
 library(shiny)
 library(shinydashboard)
+library(lubridate)
+#library(gdata)
 library(leaflet)
+
 
 ui <- dashboardPage(
   dashboardHeader(title = "COVID 19 Dashboard"),
@@ -11,7 +14,7 @@ ui <- dashboardPage(
       menuItem("Incidence, Recovery and Death", tabName = "second", icon = icon("th")),
       menuItem("Map By County", tabName = "map", icon = icon("th")),
       menuItem("Cummulative Incidence", tabName = "epicumm", icon = icon("th")),
-      h4("Last Update Date: ")
+      h4(paste("Updated on: ",Sys.Date()-days(1)))
       
     )
   ),
@@ -20,19 +23,51 @@ ui <- dashboardPage(
       tabItem(tabName = "first",
               h2(paste("COVID-19 Epidermic Curve with the Predicted Growth as at ", Sys.Date()-days(1))),
               fluidRow(
-                box("Total Confirmed cases",width =  4,
+                tags$head(tags$style(HTML("
+                                #totalCases{
+                                  text-align: center;
+                                  fonts-weight: bold;
+                                }
+                                #totalDeaths{
+                                  text-align: center;
+                                }
+                                #totalRecovered{
+                                  text-align: center;
+                                }
+                                #totalActive{
+                                  text-align: center;
+                                }
+                                div.box-header {
+                                  text-align: center;
+                                 
+                                }
+                                "))),
+                box(title = "Total Confirmed Cases",width =  3, status = "warning", solidHeader = TRUE,
+                    
                        textOutput("totalCases")),
-                box("Total Deaths", width =  4,
+                box(title = "Total Deaths (n,%)", width =  3, status = "danger",solidHeader = TRUE,
                        textOutput("totalDeaths")),
-                box("Total Recovered", width =  4,
-                       textOutput("totalRecovered"))
+                box(title = "Total Recovered (n,%)", width =  3,status = "success",solidHeader = TRUE,
+                       textOutput("totalRecovered")),
+                box(title = "Active Cases (n,%)", width =  3,status = "warning",solidHeader = TRUE,
+                    textOutput("totalActive"))
+                
+                # Dynamic infoBoxes
+                # infoBoxOutput("totalRecovered"),
+                # infoBoxOutput("approvalBox")
                 
               ),
               fluidRow(
-                box("EPI Curve", solidHeader = TRUE,
+                tags$head(tags$style(HTML("
+                                .box-header {
+                                  text-align: center;
+                                 fonts-weight: bold;
+                                }
+                                "))),
+                box(title="Epidermic Curve with Predicted Incidence and 95% CI", solidHeader = TRUE,
                     plotOutput("map1")),
                 
-                box("Daily Incidence", solidHeader = TRUE,
+                box(title="Daily Incidence", solidHeader = TRUE,
                     div(style = 'overflow-x: scroll', DT::dataTableOutput('dailyIncidence')),
                     downloadButton("download1","Download csv")
                     )
@@ -43,7 +78,7 @@ ui <- dashboardPage(
               plotOutput("plot2")
               ),
       tabItem(tabName = "map",
-              h2("Distribution of COVID-19 By County"),
+              h2("Confirmed COVID-19 Cases By County"),
               leafletOutput("testMap")
       ),
       tabItem(tabName = "epicumm",
